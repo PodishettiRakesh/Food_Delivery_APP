49e7e89cb321f3e0920cf5347ab7643d569a674b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-24 20:40:49 +0530

    imported header component in App.js

--- food_delivery/src/components/Header.jsx
+++ food_delivery/src/components/Header.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      this is header component
+      <div>
+        <ul><nav>home</nav>
+        <nav>about</nav>
+        <nav>help</nav>
+        <nav>cart</nav></ul>
+        
+      </div>
+      <p>this is message section</p>
     </div>
   )
 }
