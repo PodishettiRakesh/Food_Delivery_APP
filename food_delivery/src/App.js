6f5238adbb3c0b8b49ea6a13431dc525727eda63
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-24 22:02:02 +0530

    RestaurantList component is imported in App compoenent

--- food_delivery/src/App.js
+++ food_delivery/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+// import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import RestaurantList from './components/RestaurantList'
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <Header/>
+      <RestaurantList/>
       <Footer/>
+      
     </div>
   )
 }
--- food_delivery/src/components/RestaurantList.jsx
+++ food_delivery/src/components/RestaurantList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const RestaurantList = () => {
   return (
-    <div>
-      
-    </div>
+    <restaurantList>
+        this all are restaurants
+    </restaurantList>
   )
 }
 
