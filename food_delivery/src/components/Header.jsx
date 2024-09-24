cf1c83c72b746e41da899d412af51de2744e30a0
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-24 20:50:38 +0530

    A title is added in header component

--- food_delivery/src/components/Header.jsx
+++ food_delivery/src/components/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <ul><nav>home</nav>
-        <nav>about</nav>
-        <nav>help</nav>
-        <nav>cart</nav></ul>
-        
-      </div>
-      <p>this is message section</p>
-    </div>
+    <>
+    <h1>Food Delivery</h1>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Cart</li>
+        </ul>
+    </nav>
+    </>
   )
 }
 
