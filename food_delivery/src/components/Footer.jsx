1b52ba2a94a35fe9680574dbb73a6ad9a5e901bc
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-24 21:25:19 +0530

    nav for different links is added in footer component

--- food_delivery/src/components/Footer.jsx
+++ food_delivery/src/components/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='footer'>
+        <nav>
+            <ul className='nav-list'>
+                <li><a href='#'>Contact Us</a></li>
+                <li><a href='#'>Privacy Policy</a></li>
+                <li><a href='#'>Terms of Service</a></li>
+            </ul>
+        </nav>
+    </footer>
   )
 }
 
