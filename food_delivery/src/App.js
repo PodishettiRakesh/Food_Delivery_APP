60d1f449f4ca1b2fe51ff3251a56f841b9775cbe
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-10-07 12:16:34 +0530

    Register component is created and added styling

--- food_delivery/src/App.js
+++ food_delivery/src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 // import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-// import RestaurantList from './components/RestaurantList'
-import Login from './components/login_register/Login'
+import RestaurantList from './components/RestaurantList'
+// import Login from './components/login_register/Login'
+// import Register from './components/login_register/Register'
 
 const App = () => {
   return (
     <div className='App'>
       <Header/>
-      <Login/>
+        <RestaurantList/>
       <Footer/>
       
     </div>
