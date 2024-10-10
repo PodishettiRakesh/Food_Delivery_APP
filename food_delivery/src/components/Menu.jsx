d41b0dc285a2b611b804f15bdc5729fe3b1ab421
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-10-10 22:03:21 +0530

    menu component is modified to send cartItems as prop to cart component

--- food_delivery/src/components/Menu.jsx
+++ food_delivery/src/components/Menu.jsx
@@ -13,25 +13,41 @@ const Menu = ({ menuData }) => {
     setCartItems((prevItems) => [...prevItems, item]);
   };
 
-  
+   // Function to toggle between the cart and menu view
+   const toggleCartView = () => {
+    setViewCart(!viewCart);
+  };
+
   return (
-    <div className="menu-list">
-      {menuData.map((item, index) => (
-        <div key={index} className="menu-item">
-          {console.log(index)}
-          <img
-            className="menu-image"
-            src={item.imageUrl}
-            alt={item.name}
-          />
-          <h3 className="menu-name">{item.name}</h3>
-          <p className="menu-price">{item.price}</p>
-          <button className="add-to-cart-button">Add to Cart</button>
+    <div>
+      {/* If viewCart is true, show Cart; otherwise, show Menu */}
+      {viewCart ? (
+        <Cart cartItems={cartItems} />
+      ) : (
+        <div className="menu-list">
+          {menuData.map((item, index) => (
+            <div key={index} className="menu-item">
+              <img
+                className="menu-image"
+                src={item.imageUrl}
+                alt={item.name}
+              />
+              <h3 className="menu-name">{item.name}</h3>
+              <p className="menu-price">{item.price}</p>
+              <button
+                className="add-to-cart-button"
+                onClick={() => handleAddToCart(item)} // Add item to cart
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
-
-
-        
-      ))}
+      )}
+      {/* Toggle between Cart and Menu button */}
+      <button onClick={toggleCartView} className="view-cart-button">
+        {viewCart ? 'Go to Menu' : `View Cart (${cartItems.length})`}
+      </button>
     </div>
   );
 };
