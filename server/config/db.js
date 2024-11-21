357f91f08cd0318cf678c3b7ab0e5af2608dcfb0
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-21 22:16:40 +0530

    implemented db.js to connect with mongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Mongodb is connected');
+    } catch (error){
+        console.error('Mongodb connection error:', error.message);
+        process.exit(1);
+    }
     
-}
+};
+
+module.exports = connectDB;
