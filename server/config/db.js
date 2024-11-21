53d481f9a36fc9eca15ec589a876c429ecd9f7e1
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-21 22:37:17 +0530

    installed mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-
 const connectDB = async ()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log('Mongodb is connected');
     } catch (error){
-        console.log(MONGO_URL);
+        console.log(process.env.MONGO_URL);
         console.error('Mongodb connection error:', error.message);
         process.exit(1);
     }
