2d1da448f0f4a7623d72a659c08bb78b00c10ee3
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-28 16:16:56 +0530

    installed jsonwebtoken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -64,6 +64,7 @@ const loginUser = async (req, res) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h', // Token expiration time
     });
+    console.log(process.env.JWT_SECRET);
 
     // Send success response
     res.status(200).json({
