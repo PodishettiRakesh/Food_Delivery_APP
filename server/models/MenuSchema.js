93a1b34b51ad6efbd468a3b85d5b95af3db518e6
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 10:48:58 +0530

    Menu schema to handle the menu data

---
+++ server/models/MenuSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Menu Schema
+const MenuSchema = new mongoose.Schema({
+  restaurantId: { type: Schema.Types.ObjectId, ref: 'Restaurant', required: true }, // Links to Restaurant
+  name: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  price: { type: String, required: true },
+});
+
+const Menu = mongoose.model('Menu', MenuSchema);
+module.exports = Menu;
