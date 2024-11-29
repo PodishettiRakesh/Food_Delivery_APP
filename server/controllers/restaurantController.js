44bfabfb5976dab3fc48f7293236886cadd78b39
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 11:46:14 +0530

    getRestaurantById function is implemented

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -21,4 +21,19 @@ const addRestaurant = async (req, res) => {
     }
   };
 
+
+// Get a restaurant by ID
+const getRestaurantById = async (req, res) => {
+    const { id } = req.params; // Get restaurant ID from params
+    try {
+      const restaurant = await Restaurant.findById(id);
+      if (!restaurant) {
+        return res.status(404).json({ message: 'Restaurant not found' });
+      }
+      res.json(restaurant);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching restaurant', error });
+    }
+  };
+
   module.exports = {getAllRestaurants, addRestaurant};
