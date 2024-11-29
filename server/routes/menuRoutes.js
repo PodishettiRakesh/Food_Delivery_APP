ac32b1eeb5524b147b2a7e589e77939f089e1e7d
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 12:11:34 +0530

    addMenuToRestaurant route is defined in menuRoutes

--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const {addMenuToRestaurant,getMenuByRestaurantId} = require('../controllers/menuController');
+const {addMenuToRestaurant} = require('../controllers/menuController');
 
 // Route to add a menu to a specific restaurant
 router.post('/:restaurantId/menu', addMenuToRestaurant);
 
 // Route to get the menu of a specific restaurant
-router.get('/:restaurantId/menu', getMenuByRestaurantId);
+// router.get('/:restaurantId/menu', getMenuByRestaurantId);
 
 module.exports = router;
