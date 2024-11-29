567229721f8a62551723cde8574c1fd55a495505
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 11:35:14 +0530

    addRestaurant  route is added in restaurant

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getAllRestaurants} = require('../controllers/restaurantController');
+const {getAllRestaurants, addRestaurant} = require('../controllers/restaurantController');
 
 // Route to fetch all restaurants
 router.get('/', getAllRestaurants);
 
 // Route to add a new restaurant
-// router.post('/', addRestaurant);
+router.post('/', addRestaurant);
 
 module.exports = router;
