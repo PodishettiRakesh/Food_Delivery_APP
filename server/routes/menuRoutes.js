9838c0efd3432ba78423f99c02c7715a0aa769fb
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 14:17:29 +0530

    new route in linked to the menu routes

--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {addMenuToRestaurant} = require('../controllers/menuController');
+const {addMenuToRestaurant, getMenuByRestaurantId} = require('../controllers/menuController');
 
 // Route to add a menu to a specific restaurant
 router.post('/:restaurantId/menu', addMenuToRestaurant);
 
 // Route to get the menu of a specific restaurant
-// router.get('/:restaurantId/menu', getMenuByRestaurantId);
+router.get('/:restaurantId/menu', getMenuByRestaurantId);
 
 module.exports = router;
