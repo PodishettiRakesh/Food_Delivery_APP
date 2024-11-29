28b313c12c106a995a55138428aa912c2c3ad309
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-11-29 10:59:44 +0530

    implemented getAllRestaurants controller to fetch all resta data from mongodb

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,1 +1,12 @@
-const Restaurant = require('../models/RestaurantSchema'); 
+const Restaurant = require('../models/RestaurantSchema');
+
+const getAllRestaurants = async (req, res) => {
+    try {
+      const restaurants = await Restaurant.find(); // Fetch all restaurants
+      res.json(restaurants);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching restaurants', error });
+    }
+  };
+
+  module.exports = {getAllRestaurants};
